0e1bf3f8114bab65fdf8a99bc21b9877793d5778
Dan <danielandrewvoss@gmail.com>
2016-07-06 10:19:40 -0400

    Add comments

--- src/purchases_clojure/core.clj
+++ src/purchases_clojure/core.clj
@@ -6,16 +6,16 @@
   (println "Type a category (Furniture, Alcohol, Toiletries, Shoes, Food, Jewelry):")
   (let [purchases (slurp "purchases.csv")
         purchases (str/split-lines purchases)
-        purchases (map (fn [line] (str/split line #","))
+        purchases (map (fn [line] (str/split line #",")) ;; alt: (map #(str/split % #",") purchases)
                     purchases)
         header (first purchases)
         purchases (rest purchases)
-        purchases (map (fn [line] (zipmap header line))
+        purchases (map (fn [line] (zipmap header line)) ;; alt: (map #(zipmap header %) purchases)
                     purchases)
         category (read-line)
-        purchases (filter (fn [line] (= (get line "category") category))
+        purchases (filter (fn [line] (= (get line "category") category)) ;; (filter #(= category (get % "category")) purchases)
                     purchases)
-        file-text (pr-str purchases)]
+        file-text (pr-str purchases)] ;; to read later: read-str
     (spit "filtered_purchases.edn" file-text)))
     
         
