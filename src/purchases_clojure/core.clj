de71e11f5a266daf4f152e8efaf5e59a5c79547c
Dan <danielandrewvoss@gmail.com>
2016-07-06 14:42:48 -0400

    Turn into web app

--- src/purchases_clojure/core.clj
+++ src/purchases_clojure/core.clj
@@ -1,9 +1,11 @@
 (ns purchases-clojure.core
-  (:require [clojure.string :as str])
+  (:require [clojure.string :as str]
+            [compojure.core :as c]
+            [ring.adapter.jetty :as j]
+            [hiccup.core :as h])
   (:gen-class))
 
-(defn -main []
-  (println "Type a category (Furniture, Alcohol, Toiletries, Shoes, Food, Jewelry):")
+(defn read-purchases []
   (let [purchases (slurp "purchases.csv")
         purchases (str/split-lines purchases)
         purchases (map (fn [line] (str/split line #",")) ;; alt: (map #(str/split % #",") purchases)
@@ -11,12 +13,49 @@
         header (first purchases)
         purchases (rest purchases)
         purchases (map (fn [line] (zipmap header line)) ;; alt: (map #(zipmap header %) purchases)
-                    purchases)
-        category (read-line)
-        purchases (filter (fn [line] (= (get line "category") category)) ;; (filter #(= category (get % "category")) purchases)
-                    purchases)
-        file-text (pr-str purchases)] ;; to read later: read-str
-    (spit "filtered_purchases.edn" file-text)))
+                    purchases)]
+    purchases))
+ 
+(defn purchases-html [purchases]
+  [:html
+   [:body
+    [:a {:href "/"} "All "]
+    [:a {:href "/Furniture"} "Furniture "]
+    [:a {:href "/Alcohol"} "Alcohol "]
+    [:a {:href "/Toiletries"} "Toiletries "]
+    [:a {:href "/Shoes"} "Shoes "]
+    [:a {:href "/Food"} "Food "]
+    [:a {:href "/Jewelry"} "Jewelry "]
+    [:br]
+    [:br]
+    [:ul
+     (map (fn [line]
+            [:li (str (get line "customer_id") " " (get line "date") " " (get line "credit_card")
+                   " " (get line "cvv") " " (get line "category"))])
+       purchases)]]])
+     
+(defn filter-by-category [purchases category]
+  (filter (fn [line]
+            (= (get line "category") category))
+    purchases))
+
+(c/defroutes app
+  (c/GET "/:category{.*}" [category]
+    (let [purchases (read-purchases)
+          purchases (if (= 0 (count category))
+                      purchases
+                      (filter-by-category purchases category))]
+      (h/html (purchases-html purchases)))))
+          
+(defonce server (atom nil))
+
+(defn -main []
+  (if @server
+    (.stop @server))
+  (reset! server (j/run-jetty app {:port 3000 :join? false})))
+ 
+
+        
     
         
         
